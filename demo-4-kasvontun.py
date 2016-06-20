3f7b953750e2283b743de8d7544095a4eefafedc
neja-r <mnejar@protonmail.com>
2016-06-20 17:45:21 +0300

    Update and rename kasvontun.py to demo-4-kasvontun.py

--- demo-4-kasvontun.py
+++ demo-4-kasvontun.py
@@ -1,3 +1,4 @@
+#Lahde: http://docs.opencv.org/3.1.0/d7/d8b/tutorial_py_face_detection.html#gsc.tab=0
 import numpy as np
 import cv2
 import picamera
@@ -6,7 +7,7 @@ camera = picamera.PiCamera()
 
 face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml')  
 
-camera.capture('kasvokuva1.jpg')
+camera.capture('kasvokuva1.jpg') #Otetaan pikameralla kuva, josta etsitaan kasvot (bonustehtava 8)
 #img = cv2.imread('johnnydepp.jpg')
 img = cv2.imread('kasvokuva1.jpg')
 
@@ -20,9 +21,6 @@ for (x,y,w,h) in faces:
  cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
  roi_gray = gray[y:y+h, x:x+w]
  roi_color = img[y:y+h, x:x+w]
-         #eyes = eye_cascade.detectMultiScale(roi_gray)
-         #for (ex,ey,ew,eh) in eyes:
-         #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
      
-cv2.imwrite('kasvokuva.png',img)
+cv2.imwrite('kasvokuva.png',img) #tallennetaan kuva
 cv2.waitKey(0)
